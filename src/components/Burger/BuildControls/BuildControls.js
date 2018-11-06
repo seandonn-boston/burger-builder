45882959a33a1a92e159b88f0ab36a734cc13b47
SeanDonnellanWDI <sean.donnellan.wdi@gmail.com>
2018-11-06 15:54:21 -0500

    Create BuildControls and BuildControl components
    This commit sets up the BuildControls component with .js and .css
    The BuildControls component will be made up of multiple BuildControl
    components
    BuildControl has a .js and .css

---
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import classes from './BuildControls.css'
+
+const buildControls = (props) => (
+  <div classname={classes.BuildControls}>
+    
+  </div>
+);
+
+export default buildControls;
