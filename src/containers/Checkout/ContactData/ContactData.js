69f2486a485eed36ad7bba0eb1327528012851b6
SeanDonnellanWDI <sean.donnellan.wdi@gmail.com>
2018-11-08 16:18:04 -0500

    Order Submission and Passing Data between pages
    Create orderHandler, access ingredients, contact data, and price
    add loading to ContactData state
    orderHandler is essentially the old BurgerBuilder purchaseContinueHandler
    adjust parameters and values, pass all necessary values from Checkout to ContactData
    import Spinner, load conditionally
    redirect user to "/" when order now POST request is successful

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
+import axios from '../../../axios-orders';
 
 import Button from '../../../components/UI/Button/Button';
-
+import Spinner from '../../../components/UI/Spinner/Spinner';
 import classes from './ContactData.css';
 
 class ContactData extends Component {
@@ -15,24 +16,60 @@ class ContactData extends Component {
       zipCode: '',
       country: ''
     },
-    deliveryMethod: ''
+    deliveryMethod: '',
+    loading: false
+  }
+
+  orderHandler = (event) => {
+    event.preventDefault();
+    this.setState({loading: true});
+    const order = {
+      ingredients: this.props.ingredients,
+      price: this.props.price,
+      customer: {
+        name: 'Sean Donn',
+        email: 'sean@seandonn.io',
+        address: {
+          street: '125 Summer Street',
+          city: 'Boston',
+          state: 'MA',
+          zipCode: '02110',
+          country: 'USA'
+        }
+      },
+      deliveryMethod: 'fastest'
+    }
+    axios.post('/orders.json', order)
+      .then(res => {
+          this.setState({loading: false});
+          this.props.history.push('/');
+      })
+      .catch(err => {
+          this.setState({loading: false});
+      });
   }
 
   render() {
+    let form = (
+      <form>
+        <input className={classes.Input} type="text" name="name" placeholder="Your Name" />
+        <input className={classes.Input} type="email" name="email" placeholder="Your Email" />
+        <input className={classes.Input} type="text" name="street" placeholder="Your Street" />
+        <input className={classes.Input} type="text" name="city" placeholder="Your City" />
+        <input className={classes.Input} type="text" name="state" placeholder="Your State" />
+        <input className={classes.Input} type="text" name="zipcode" placeholder="Your Zip Code" />
+        <input className={classes.Input} type="text" name="country" placeholder="Your Country" />
+        <input className={classes.Input} type="text" name="delivery method" placeholder="Your Delivery Method preferencwe" />
+        <Button btnType="Success" clicked={this.orderHandler}>ORDER NOW</Button>
+      </form>
+    );
+    if (this.state.loading) {
+      form = <Spinner />;
+    }
     return (
       <div className={classes.ContactData}>
         <h4>Enter your contact data!</h4>
-        <form>
-          <input className={classes.Input} type="text" name="name" placeholder="Your Name" />
-          <input className={classes.Input} type="email" name="email" placeholder="Your Email" />
-          <input className={classes.Input} type="text" name="street" placeholder="Your Street" />
-          <input className={classes.Input} type="text" name="city" placeholder="Your City" />
-          <input className={classes.Input} type="text" name="state" placeholder="Your State" />
-          <input className={classes.Input} type="text" name="zipcode" placeholder="Your Zip Code" />
-          <input className={classes.Input} type="text" name="country" placeholder="Your Country" />
-          <input className={classes.Input} type="text" name="delivery method" placeholder="Your Delivery Method preferencwe" />
-          <Button btnType="Success">ORDER NOW</Button>
-        </form>
+        {form}
       </div>
     );
   };
