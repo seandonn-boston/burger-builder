4b029f36bd0d179a0c291d9ef1dafc5fe577cf03
SeanDonnellanWDI <sean.donnellan.wdi@gmail.com>
2018-11-08 17:11:38 -0500

    Outputting the Orders Dynamically
    pass the key, ingredients, and price
    dynamically display ingredient name, value, and price in Order.js
    style each output

--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -2,11 +2,28 @@ import React from 'react';
 
 import classes from './Order.css'
 
-const order = (props) => (
-  <div className={classes.Order}>
-    <p>Ingredients: Salad (1)</p>
-    <p>Price: <strong>$4.50</strong></p>
-  </div>
-);
+const order = (props) => {
+  const ingredients = [];
+
+  for (let ingredientName in props.ingredients) {
+    ingredients.push({
+      name: ingredientName,
+      amount: props.ingredients[ingredientName]
+    });
+  }
+
+  const ingredientOutput = ingredients.map(ig => {
+    return <span
+      key={ig.name}
+      className={classes.IngredientOutput}>{ig.name} ({ig.amount})</span>
+  });
+
+  return (
+    <div className={classes.Order}>
+      <p>Ingredients: {ingredientOutput}</p>
+      <p>Price: <strong>${Number.parseFloat(props.price).toFixed(2)}</strong></p>
+    </div>
+  );
+};
 
 export default order;
