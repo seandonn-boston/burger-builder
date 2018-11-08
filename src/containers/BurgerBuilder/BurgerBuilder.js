55a27cdb65e60e3c4d1bebd81e68748620aced48
SeanDonnellanWDI <sean.donnellan.wdi@gmail.com>
2018-11-08 13:23:18 -0500

    Handling Errors
    setup a global error handler which shows a modal
    create hoc/withErrorHandler/withErrorHandler.js
    import and use in BurgerBuilder
    show modal according to error which is set in request/response
    interceptors inside componentDidMount

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import axios from '../../axios-orders';
 
+import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
+
 import Aux from '../../hoc/Aux/Aux';
 import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
@@ -148,4 +150,4 @@ class BurgerBuilder extends Component {
   }
 }
 
-export default BurgerBuilder;
+export default withErrorHandler(BurgerBuilder, axios);
