7ff46a7be91d3db39e99965e3f364fcbdac6ed50
SeanDonnellanWDI <sean.donnellan.wdi@gmail.com>
2018-11-09 15:00:12 -0500

    Handling overall form validity
    turn off the order button if the form is invalid
    add formIsValid to state (outside of orderForm)
    check if true/false inside inputChangedHandler, update state
    bind the order now button disabled attribute to formIsValid
    pass the disabled property to the Button.js
    add valid: true to the dropdown orderForm state nodes to make sure form
    validity can check for a valid property in every loop
    update the styling for a disabled button

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -110,9 +110,11 @@ class ContactData extends Component {
             {value: 'cheapest', displayValue: 'Cheapest'}
           ]
         },
-        value: ''
+        value: '',
+        valid: true
       }
     },
+    formIsValid: false,
     loading: false
   }
 
@@ -149,8 +151,11 @@ class ContactData extends Component {
     updatedFormElement.valid = this.checkValidity(updatedFormElement.value, updatedFormElement.validation);
     updatedFormElement.touched = true;
     updatedOrderForm[inputIdentifier] = updatedFormElement;
-    console.log(updatedFormElement);
-    this.setState({orderForm: updatedOrderForm});
+    let formIsValid = true;
+    for (let inputIdentifier in updatedOrderForm) {
+      formIsValid = updatedOrderForm[inputIdentifier].valid && formIsValid;
+    }
+    this.setState({orderForm: updatedOrderForm, formIsValid: formIsValid});
   }
 
   checkValidity(value, rules) {
@@ -190,7 +195,7 @@ class ContactData extends Component {
             shouldValidate={formElement.config.validation}
             changed={(event) => this.inputChangedHandler(event, formElement.id)} />
         ))}
-        <Button btnType="Success">ORDER NOW</Button>
+        <Button btnType="Success" disabled={!this.state.formIsValid}>ORDER NOW</Button>
       </form>
     );
     if (this.state.loading) {
