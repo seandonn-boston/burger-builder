a768d478534b127fbe5255a7b38576b394208685
SeanDonnellanWDI <sean.donnellan.wdi@gmail.com>
2018-11-09 16:13:21 -0500

    Finishing the reducer for ingredients
    create the switch/case handler for ingredient add and remove methods
    comment out the componentDidMount logic in BurgerBuilder because we will
    not handle asynchronous methods with redux yet.

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,12 +1,37 @@
 import * as actionTypes from './actions';
 
 const initialState = {
-  ingredients: null,
+  ingredients: {
+    salad: 0,
+    bacon: 0,
+    cheese: 0,
+    meat: 0
+  },
   totalPrice: 4
 }
 
 const reducer = (state = initialState, action) => {
-
+  switch (action.type) {
+    case actionTypes.ADD_INGREDIENT:
+      return {
+        ...state,
+        ingredients: {
+          ...state.ingredients,
+          [action.ingredientName]: state.ingredients[action.ingredientName] + 1
+          // [action.ingredientName] is not an array, it is a special ES6 syntax to pass a key as a parameter and access it
+        }
+      };
+    case actionTypes.REMOVE_INGREDIENT:
+      return {
+        ...state,
+        ingredients: {
+          ...state.ingredients,
+          [action.ingredientName]: state.ingredients[action.ingredientName] - 1
+        }
+      };
+    default:
+      return state;
+  }
 }
 
 export default reducer;
