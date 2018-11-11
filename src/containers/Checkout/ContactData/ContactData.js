6084e6445db58f609bef639a5f61ef732d4e6169
SeanDonnellanWDI <sean.donnellan.wdi@gmail.com>
2018-11-11 16:57:44 -0500

    Adding Order actionTypes and Action Creators
    add PURCHASE_BURGER_SUCCESS and PURCHASE_BURGER_FAIL to actionTypes.js
    add the actionCreators for each in actions/order.js
    import axios (thus allowing contact with the server)
    literally cut and paste the axious post and .tehn and .catch from
    contactData.js orderHandler into a new actionCreator purchaseBurgerStart
    reconfigure purchaseBurgerStart to handle the purchaseBurgerSuccess and
    purchaseBurgerFailactionCreators

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -132,14 +132,6 @@ class ContactData extends Component {
       price: this.props.price,
       orderData: formData
     }
-    axios.post('/orders.json', order)
-      .then(res => {
-          this.setState({loading: false});
-          this.props.history.push('/');
-      })
-      .catch(err => {
-          this.setState({loading: false});
-      });
   }
 
   inputChangedHandler = (event, inputIdentifier) => {
