846716258b10ee2cc33ee2e1f1d94525c8e76795
SeanDonnellanWDI <sean.donnellan.wdi@gmail.com>
2018-11-12 08:20:13 -0500

    Add form validation and 2way binding
    create a checkValidity method to Auth.js
    Add an event listener to listen for event inputs and display them as the
    form input values
    Adjust the validity checker in contactData to validate against some
    regEx's

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -33,7 +33,8 @@ class ContactData extends Component {
         },
         value: '',
         validation: {
-          required: true
+          required: true,
+          isEmail: true
         },
         valid: false,
         touched: false
@@ -87,7 +88,8 @@ class ContactData extends Component {
         validation: {
           required: true,
           minLength: 5,
-          maxLength: 5
+          maxLength: 5,
+          isNumeric: true
         },
         valid: false,
         touched: false
@@ -158,14 +160,22 @@ class ContactData extends Component {
     if (!rules) {
       return true;
     }
-    if (rules.required && isValid) {
-      isValid = value.trim() !== '';
+    if (rules.required) {
+      isValid = value.trim() !== '' && isValid;
     }
-    if (rules.minLength && isValid) {
-      isValid = value.length >= rules.minLength;
+    if (rules.minLength) {
+      isValid = value.length >= rules.minLength && isValid
     }
-    if (rules.maxLength && isValid) {
-      isValid = value.length <= rules.maxLength;
+    if (rules.maxLength) {
+      isValid = value.length <= rules.maxLength && isValid
+    }
+    if (rules.isEmail) {
+      const pattern = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
+      isValid = pattern.test(value) && isValid
+    }
+    if (rules.isNumeric) {
+      const pattern = /^\d+$/;
+      isValid = pattern.test(value) && isValid
     }
     return isValid;
   }
