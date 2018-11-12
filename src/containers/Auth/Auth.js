0c019fd2a719046dd3be5917a4916b8156d6e418
SeanDonnellanWDI <sean.donnellan.wdi@gmail.com>
2018-11-12 09:41:14 -0500

    Create the sign in functionality
    Provide a toggle between signup and login
    switch between the two according to a button click
    change the header and button content according to this state
    change the POST url according to this state as well

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -37,7 +37,8 @@ class Auth extends Component {
         valid: false,
         touched: false
       }
-    }
+    },
+    isSignup: true
   }
 
   checkValidity(value, rules) {
@@ -80,7 +81,13 @@ class Auth extends Component {
 
   submitHandler = (event) => {
     event.preventDefault();
-    this.props.onAuth(this.state.controls.email.value, this.state.controls.password.value)
+    this.props.onAuth(this.state.controls.email.value, this.state.controls.password.value, this.state.isSignup)
+  }
+
+  switchAuthModeHandler = () => {
+    this.setState(prevState => {
+      return {isSignup: !prevState.isSignup};
+    });
   }
 
   render () {
@@ -107,10 +114,14 @@ class Auth extends Component {
     })
     return (
       <div className={classes.Auth}>
+        <h3>{this.state.isSignup ? 'Sign Up' : 'Login'}</h3>
         <form onSubmit={this.submitHandler}>
           {form}
           <Button btnType="Success">SUBMIT</Button>
         </form>
+        <Button
+          clicked={this.switchAuthModeHandler}
+          btnType="Danger">SWITCH TO {this.state.isSignup ? 'LOGIN' : 'SIGN UP'}</Button>
       </div>
     );
   }
@@ -118,7 +129,7 @@ class Auth extends Component {
 
 const mapDispatchToProps = dispatch => {
   return {
-    onAuth: (email, password) => dispatch(actions.auth(email, password))
+    onAuth: (email, password, isSignup) => dispatch(actions.auth(email, password, isSignup))
   }
 }
 
--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -22,7 +22,7 @@ export const authFail = (error) => {
   };
 };
 
-export const auth = (email, password) => {
+export const auth = (email, password, isSignup) => {
   return dispatch => {
     dispatch(authStart());
     const authData = {
@@ -30,7 +30,11 @@ export const auth = (email, password) => {
       password: password,
       returnSecureToken: true
     }
-    axios.post('https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=AIzaSyBGU-2TeiBQTD8hDYMD9UZ-mLsmnZcMSRk', authData)
+    let url = 'https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=AIzaSyBGU-2TeiBQTD8hDYMD9UZ-mLsmnZcMSRk'
+    if (!isSignup) {
+      url = 'https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword?key=AIzaSyBGU-2TeiBQTD8hDYMD9UZ-mLsmnZcMSRk';
+    }
+    axios.post(url, authData)
     .then(res => {
       console.log(res);
       dispatch(authSuccess(res.data));
