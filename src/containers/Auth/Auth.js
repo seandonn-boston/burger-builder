7cbaa3909b933d7da21983ca96a479162718c7fd
SeanDonnellanWDI <sean.donnellan.wdi@gmail.com>
2018-11-12 14:09:50 -0500

    Adding UI feedback
    import Spinner in Auth container
    add mapStateToProps at the bottom, map loading
    pass mapStateToProps to connect
    render either the form or the spinner if loading is true or false
    let's also see an error message if we see wrong data
    use the default error message provided by firebase
    add error to mapStateToProps, use it to conditionally render the error
    message

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -3,6 +3,7 @@ import { connect } from 'react-redux';
 
 import Input from '../../components/UI/Input/Input';
 import Button from '../../components/UI/Button/Button';
+import Spinner from '../../components/UI/Spinner/Spinner';
 import classes from './Auth.css';
 import * as actions from '../../store/actions/index';
 
@@ -99,7 +100,7 @@ class Auth extends Component {
         config: this.state.controls[key]
       });
     }
-    const form = formElementsArray.map(formElement => {
+    let form = formElementsArray.map(formElement => {
       return <Input
         key={formElement.id}
         name={formElement.name}
@@ -111,10 +112,21 @@ class Auth extends Component {
         shouldValidate={formElement.config.validation}
         changed={(event) => this.inputChangedHandler(event, formElement.id)}
         />
-    })
+    });
+    if (this.props.loading) {
+      form = <Spinner />
+    }
+    let errorMessage = null;
+    if (this.props.error) {
+      errorMessage = (
+        <p>{this.props.error.message}</p>
+      )
+    }
+
     return (
       <div className={classes.Auth}>
         <h3>{this.state.isSignup ? 'Sign Up' : 'Login'}</h3>
+        {errorMessage}
         <form onSubmit={this.submitHandler}>
           {form}
           <Button btnType="Success">SUBMIT</Button>
@@ -127,10 +139,17 @@ class Auth extends Component {
   }
 }
 
+const mapStateToProps = state => {
+  return {
+    loading: state.auth.loading,
+    error: state.auth.error
+  }
+}
+
 const mapDispatchToProps = dispatch => {
   return {
     onAuth: (email, password, isSignup) => dispatch(actions.auth(email, password, isSignup))
   }
 }
 
-export default connect(null, mapDispatchToProps)(Auth);
+export default connect(mapStateToProps, mapDispatchToProps)(Auth);
