f9b0087ab44958490511447640278d94b26c3b04
SeanDonnellanWDI <sean.donnellan.wdi@gmail.com>
2018-11-12 16:54:57 -0500

    Forwarding unauthenticated users
    conditionally show Orders navigation Item if authenticated
    upon successful login, redirect user to "/" and do so inside the Auth
    container
    If logged out, display signup to continue before allowing a customer to
    continue to a checkout page
    do so by conditionally rendering the content of the order now button
    handle the redirect via history.push in the purchaseHandler of the
    BurgerBuilder

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Redirect } from 'react-router-dom';
 
 import Input from '../../components/UI/Input/Input';
 import Button from '../../components/UI/Button/Button';
@@ -122,9 +123,14 @@ class Auth extends Component {
         <p>{this.props.error.message}</p>
       )
     }
+    let authRedirect = null;
+    if (this.props.isAuthenticated) {
+      authRedirect = <Redirect to="/" />
+    }
 
     return (
       <div className={classes.Auth}>
+        {authRedirect}
         <h3>{this.state.isSignup ? 'Sign Up' : 'Login'}</h3>
         {errorMessage}
         <form onSubmit={this.submitHandler}>
@@ -142,7 +148,8 @@ class Auth extends Component {
 const mapStateToProps = state => {
   return {
     loading: state.auth.loading,
-    error: state.auth.error
+    error: state.auth.error,
+    isAuthenticated: state.auth.token !== null
   }
 }
 
