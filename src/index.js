25e58e75f033e587422e4b05cd8f32442a09cdc4
SeanDonnellanWDI <sean.donnellan.wdi@gmail.com>
2018-11-13 16:04:40 -0500

    Slight adjustment to basename

--- src/index.js
+++ src/index.js
@@ -26,7 +26,7 @@ const store = createStore(rootReducer, composeEnhancers(
 
 const app = (
   <Provider store={store}>
-    <BrowserRouter basename="/burger-builder/">
+    <BrowserRouter basename="/burger-builder">
       <App />
     </BrowserRouter>
   </Provider>
